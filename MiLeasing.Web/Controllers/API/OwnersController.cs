3e47bfcfde161caa244df3984e454b75ebc3f287
Horacio Xochitemol Bautista <microbiano@gmail.com>
2020-07-30 21:42:53 -0500

    OwnerAPiController

--- MiLeasing.Web/Controllers/API/OwnersController.cs
+++ MiLeasing.Web/Controllers/API/OwnersController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using MiLeasing.Common.Models;
 using MiLeasing.Web.Data;
+using MiLeasing.Web.Data.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,24 +21,87 @@ namespace MiLeasing.Web.Controllers.API
             _dataContext = dataContext;
         }
 
+
         [HttpPost]
         [Route("GetOwnerByEmail")]
-        public async Task<IActionResult> GetOwnerByEmailAsync(EmailRequest Request)
+        public async Task<IActionResult> GetOwnerByEmailAsync(EmailRequest request)
         {
-            if (!ModelState.IsValid) 
+            if (!ModelState.IsValid)
             {
                 return BadRequest();
             }
 
             var owner = await _dataContext.Owners
-                       .FirstOrDefaultAsync(o=>o.User.Email.ToLower() == Request.Email.ToLower());
+                .Include(o => o.User)
+                .Include(o => o.Properties)
+                .ThenInclude(p => p.PropertyType)
+                .Include(o => o.Properties)
+                .ThenInclude(p => p.PropertyImages)
+                .Include(o => o.Contracts)
+                .ThenInclude(c => c.Lessee)
+                .ThenInclude(l => l.User)
+                .FirstOrDefaultAsync(o => o.User.Email.ToLower() == request.Email.ToLower());
 
             if (owner == null)
             {
                 return NotFound();
             }
 
-            return Ok(owner);
+            var response = new OwnerResponse
+            {
+                Id = owner.Id,
+                FirstName = owner.User.FirstName,
+                LastName = owner.User.LastName,
+                Address = owner.User.Address,
+                Document = owner.User.Document,
+                Email = owner.User.Email,
+                PhoneNumber = owner.User.PhoneNumber,
+                Properties = owner.Properties?.Select(p => new PropertyResponse
+                {
+                    Address = p.Address,
+                    Contracts = p.Contracts?.Select(c => new ContractResponse
+                    {
+                        EndDate = c.EndDate,
+                        Id = c.Id,
+                        IsActive = c.IsActive,
+                        Lessee = ToLessesResponse(c.Lessee),
+                        Price = c.Price,
+                        Remarks = c.Remarks,
+                        StartDate = c.StartDate
+                    }).ToList(),
+                    HasParkingLot = p.HasParkingLot,
+                    Id = p.Id,
+                    IsAvailable = p.IsAvailable,
+                    Neighborhood = p.Neighborhood,
+                    Price = p.Price,
+                    PropertyImages = p.PropertyImages?.Select(pi => new PropertyImageResponse
+                    {
+                        Id = pi.Id,
+                        ImageUrl = pi.ImageFullPath
+                    }).ToList(),
+                    PropertyType = p.PropertyType.Name,
+                    Remarks = p.Remarks,
+                    Rooms = p.Rooms,
+                    SquareMeters = p.SquareMeters,
+                    Stratum = p.Stratum
+                }).ToList()
+            };
+
+            return Ok(response);
+        }
+
+        private LesseeResponse ToLessesResponse(Lessee lessee)
+        {
+            return new LesseeResponse
+            {
+                Id = lessee.Id,
+                Address = lessee.User.Address,
+                Document = lessee.User.Document,
+                Email = lessee.User.Email,
+                FirstName = lessee.User.FirstName,
+                LastName = lessee.User.LastName,
+                PhoneNumber = lessee.User.PhoneNumber
+            };
         }
     }
 }
--- MiLeasing.Web/Controllers/HomeController.cs
+++ MiLeasing.Web/Controllers/HomeController.cs
@@ -48,3 +48,4 @@ namespace MiLeasing.Web.Controllers
 //Todo:https://www.youtube.com/watch?v=gKJw0J2NV0A&list=PLuEZQoW9bRnRkiC9j5XkMVphyWkz0cdRk&index=15
 //Todo:https://www.youtube.com/watch?v=l4fm65iPTlk&list=PLuEZQoW9bRnRkiC9j5XkMVphyWkz0cdRk&index=19
 //Todo:https://www.youtube.com/watch?v=T3-ClayCh6Q&list=PLuEZQoW9bRnRkiC9j5XkMVphyWkz0cdRk&index=23
+//Todo:https://www.youtube.com/watch?v=T3-ClayCh6Q&list=PLuEZQoW9bRnRkiC9j5XkMVphyWkz0cdRk&index=23
